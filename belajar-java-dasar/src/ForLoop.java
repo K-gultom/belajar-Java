ca7959d3ff22b59fd20f4472e01361db729fe42c
K-gultom <yehezkielgg28@gmail.com>
2023-01-19 09:39:02 +0700

    commit 3

--- belajar-java-dasar/src/ForLoop.java
+++ belajar-java-dasar/src/ForLoop.java
@@ -1,13 +1,19 @@
 public class ForLoop {
     public static void main(String[] args) {
+        int a,b;
 
-
-        var counter = 1;
-        for (;counter <= 10;){
-            System.out.println("Perulangan " + counter);
-
-            counter++;
+        for (a = 1; a <= 5; a++){
+            for (b = 1; b <= a; b++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
-
     }
 }
+
+// Below is Materi Pak Eko Khanedy
+//        var counter = 1;
+//        for (;counter <= 10;){
+//            System.out.println("Perulangan " + counter);
+//            counter++;
+//        }
