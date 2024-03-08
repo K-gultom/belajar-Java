42dc6fe1f48bcefd9c5983f238585271d03b92b8
K-gultom <yehezkielgg28@gmail.com>
2024-03-08 08:27:21 +0700

    08/03/24

--- HelloWorld/src/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        var name = 20;
-        System.out.println(name);
-    }
-}
